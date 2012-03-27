0ce660a810854bd245d2ff5b7215aa23af5022e1
Curtis Thibault <curtis.thibault@gmail.com>
2012-03-26 19:02:41 -0500

    Updated Web Config

--- DailyScores/Models/EmailAddress.cs
+++
@@ -1,9 +0,0 @@
-namespace DailyScores.Models
-{
-    public class EmailAddress
-    {
-        public int AddressId { get; set; }
-        public int PlayerId { get; set; }
-        public string Address { get; set; }
-    }
-}
