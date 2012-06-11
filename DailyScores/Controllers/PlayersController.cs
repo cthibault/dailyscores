057e3127a66f3ca1dac266be8674496073bd476b
Curtis Thibault <curtis.thibault@gmail.com>
2012-06-11 14:53:22 -0500

    Added basic Player statistics

--- DailyScores/Controllers/PlayersController.cs
+++ DailyScores/Controllers/PlayersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,18 +26,41 @@ namespace DailyScores.Controllers
         {
             Player player = this.Repository.Players.Find(id);
 
-            ViewBag.HidatoScores = this.Repository.HidatoScores
+            ViewBag.HidatoScores = this.GetHidatoScores(id, 10);
+            ViewBag.JumbleScores = this.GetJumbleScores(id, 10);
+            ViewBag.MissingScores = this.GetMissingScores(id, 20);
+            ViewBag.EmailSubmissions = this.GetEmailSubmissions(player, 10);
+            ViewBag.Statistics = this.GetStatistics(id);
+            
+            return View(player);
+        }
+
+        #region Get Scores
+
+        private List<HidatoScore> GetHidatoScores(int id, int count)
+        {
+            return this.Repository.HidatoScores
                 .Where(s => s.PlayerId == id)
                 .OrderByDescending(s => s.HidatoId)
-                .Take(10)
+                .Take(count)
                 .ToList();
+        }
 
-            ViewBag.JumbleScores = this.Repository.JumbleScores
+        private List<JumbleScore> GetJumbleScores(int id, int count)
+        {
+            return this.Repository.JumbleScores
                 .Where(s => s.PlayerId == id)
                 .OrderByDescending(s => s.JumbleId)
-                .Take(10)
+                .Take(count)
                 .ToList();
+        }
 
+        #endregion Get Scores
+
+        #region Get Missing Scores
+
+        private List<MissingScore> GetMissingScores(int id, int count)
+        {
             var missingScoresQuery = this.Repository.HidatoScores
                 .GroupBy(hs => hs.Date)
                 .Where(g => g.All(s => s.PlayerId != id))
@@ -59,17 +83,120 @@ namespace DailyScores.Controllers
                                   })
                 );
 
-            ViewBag.MissingScores = missingScoresQuery
+            return missingScoresQuery
                 .OrderByDescending(ms => ms.Date)
-                .Take(20);
-            
-            ViewBag.EmailSubmissions = this.Repository.EmailSubmissions.ToList()
+                .Take(count)
+                .ToList();
+        }
+
+        #endregion Get Missing Scores
+
+        #region Get Email Submissions
+
+        private List<EmailSubmission> GetEmailSubmissions(Player player, int count)
+        {
+            return this.Repository.EmailSubmissions.ToList()
                 .Where(s => player.EmailAddresses.Any(ea => s.From == ea.Address))
                 .OrderByDescending(s => s.EmailSubmissionId)
-                .Take(10)
+                .Take(count)
                 .ToList();
-
-            return View(player);
         }
+
+        #endregion Get Email Submissions
+
+        #region Get Statistics
+
+        private List<Statistics> GetStatistics(int id)
+        {
+            var list = new List<Statistics>
+                       {
+                           this.GetHidatoStatistics(id), 
+                           this.GetJumbleStatistics(id)
+                       };
+
+            return list;
+        }
+
+        private Statistics GetHidatoStatistics(int id)
+        {
+            var groupedHidatoScores = from score in this.Repository.HidatoScores
+                                      group score by score.Date
+                                      into scoreGroup
+                                      let list = scoreGroup.OrderByDescending(s => s.TotalScore).ThenBy(s => s.TimeInSeconds)
+                                      let best = scoreGroup.OrderByDescending(s => s.TotalScore).ThenBy(s => s.TimeInSeconds).FirstOrDefault()
+                                      let tied = scoreGroup.Where(s => s.TotalScore == best.TotalScore && s.TimeInSeconds == best.TimeInSeconds)
+                                      select new
+                                             {
+                                                 Date = scoreGroup.Key, 
+                                                 Results = list, 
+                                                 Best = best,
+                                                 IsTie = tied.Count() > 1 && tied.Any(s => s.PlayerId == id)
+                                             };
+
+            //foreach (var g in groupedHidatoScores)
+            //{
+            //    Debug.Print("{0}: [{1}] {2}", g.Date, g.Best.HidatoId, g.Tie);
+            //    foreach (var s in g.Results)
+            //    {
+            //        Debug.Print(" {0}: {1}, {2}", s.HidatoId, s.TotalScore, s.TimeInSeconds);
+            //    }
+            //}
+
+            var statistics = new Statistics
+                             {
+                                 Title = "Hidato",
+                                 GamesPlayed = groupedHidatoScores.SelectMany(g => g.Results).Count(s => s.PlayerId == id),
+                                 Wins = groupedHidatoScores.Count(g => !g.IsTie && g.Best.PlayerId == id),
+                                 Losses = groupedHidatoScores.Count(g => !g.IsTie && g.Best.PlayerId != id),
+                                 Ties = groupedHidatoScores.Count(g => g.IsTie),
+                                 HighScore = groupedHidatoScores.SelectMany(g => g.Results).Max(s => s.TotalScore),
+                                 LowScore = groupedHidatoScores.SelectMany(g => g.Results).Min(s => s.TotalScore),
+                                 AverageScore = Math.Round(groupedHidatoScores.SelectMany(g => g.Results).Average(s => s.TotalScore), 2)
+                             };
+
+            return statistics;
+        }
+
+        private Statistics GetJumbleStatistics(int id)
+        {
+            var groupedJumbleScores = from score in this.Repository.JumbleScores
+                                      group score by score.Date
+                                      into scoreGroup
+                                      let list = scoreGroup.OrderByDescending(s => s.TotalScore).ThenBy(s => s.TimeInSeconds)
+                                      let best = scoreGroup.OrderByDescending(s => s.TotalScore).ThenBy(s => s.TimeInSeconds).FirstOrDefault()
+                                      let tied = scoreGroup.Where(s => s.TotalScore == best.TotalScore && s.TimeInSeconds == best.TimeInSeconds)
+                                      select new
+                                             {
+                                                 Date = scoreGroup.Key, 
+                                                 Results = list, 
+                                                 Best = best,
+                                                 IsTie = tied.Count() > 1 && tied.Any(s => s.PlayerId == id)
+                                             };
+
+            //foreach (var g in groupedHidatoScores)
+            //{
+            //    Debug.Print("{0}: [{1}] {2}", g.Date, g.Best.HidatoId, g.Tie);
+            //    foreach (var s in g.Results)
+            //    {
+            //        Debug.Print(" {0}: {1}, {2}", s.HidatoId, s.TotalScore, s.TimeInSeconds);
+            //    }
+            //}
+
+            var statistics = new Statistics
+                             {
+                                 Title = "Jumble",
+                                 GamesPlayed = groupedJumbleScores.SelectMany(g => g.Results).Count(s => s.PlayerId == id),
+                                 Wins = groupedJumbleScores.Count(g => !g.IsTie && g.Best.PlayerId == id),
+                                 Losses = groupedJumbleScores.Count(g => !g.IsTie && g.Best.PlayerId != id),
+                                 Ties = groupedJumbleScores.Count(g => g.IsTie),
+                                 HighScore = groupedJumbleScores.SelectMany(g => g.Results).Max(s => s.TotalScore),
+                                 LowScore = groupedJumbleScores.SelectMany(g => g.Results).Min(s => s.TotalScore),
+                                 AverageScore = Math.Round(groupedJumbleScores.SelectMany(g => g.Results).Average(s => s.TotalScore), 2)
+                             };
+
+            return statistics;
+        }
+
+        #endregion Get Statistics
     }
 }
---
+++ DailyScores/Models/Statistics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyScores.Models
+{
+    public class Statistics
+    {
+        public string Title { get; set; }
+
+        public int GamesPlayed { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Ties { get; set; }
+        public int HighScore { get; set; }
+        public int LowScore { get; set; }
+        public double AverageScore { get; set; }
+        public DateTime Range { get; set; }
+    }
+}
